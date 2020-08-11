7fbac4b232056ad3acaa4792640a780c97a78272
John Behan <behanj@gmail.com>
2020-08-11 18:09:06 +0100

    update base path

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,11 +1,13 @@
 const merge = require('webpack-merge');
 const base = require('./base');
 const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
 
 module.exports = merge(base, {
   mode: 'production',
   output: {
     filename: 'bundle.min.js',
+    path: path.resolve(__dirname, '../dist'),
   },
   devtool: false,
   performance: {
